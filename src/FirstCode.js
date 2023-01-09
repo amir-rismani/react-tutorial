146686c8c813b1ee84456ccb3b169cbf5c7062df
Amir Rismaani <amir.rismaani@gmail.com>
2023-01-09 19:57:34 +0330

    Separate first React code

---
+++ src/FirstCode.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+
+const element = React.createElement("div", { id: "identify", className: "container" }, "First React Code!");
+const container = document.getElementById('root');
+
+// ReactDOM.render is no longer supported in React 18.
+// ReactDOM.render(element, container);
+
+// Use createRoot instead ReactDOM.render
+const root = createRoot(container);
+
+// Render Element
+root.render(element);
