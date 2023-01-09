a1c0bee42c811a8e3811effe3dc615bb363bced6
Amir Rismaani <amir.rismaani@gmail.com>
2023-01-09 22:53:57 +0330

    Class base component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
+import { Component } from "react";
 import Products from "./components/Products/Products";
-
-const App = () => {
-    return (
-        <div>
-            <Products />
-        </div>
-    );
+// Class base component
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Products />
+            </div>
+        );
+    }
 }
+
+// Function base component
+// const App = () => {
+//     return (
+//         <div>
+//             <Products />
+//         </div>
+//     );
+// }
 
 export default App;
