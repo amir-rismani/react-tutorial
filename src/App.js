1ac1ccd7c281ebefbeb5c6f17c193be96c3a9499
Amir Rismaani <amir.rismaani@gmail.com>
2023-01-09 20:02:59 +0330

    App component development

---
+++ src/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return ( 
+        <div>
+            First React App!
+        </div>
+     );
+}
+ 
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
-import React from 'react';
-// import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
-const element = React.createElement("div", { id: "identify", className: "container" }, "First React Code!");
+import App from './App';
+
 const container = document.getElementById('root');
-
-// ReactDOM.render is no longer supported in React 18.
-// ReactDOM.render(element, container);
-
-// Use createRoot instead ReactDOM.render
 const root = createRoot(container);
-root.render(element);
+root.render(<App />);
