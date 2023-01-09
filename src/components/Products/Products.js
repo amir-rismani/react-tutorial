d0341e3334130971cf73d4a8688bdbb5831ecdf1
Amir Rismaani <amir.rismaani@gmail.com>
2023-01-09 23:15:13 +0330

    Use state in class and function based component

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,15 +1,61 @@
+import { Component, useState } from "react";
 import Product from "../Product/Product";
 
+// Class based component
+// class Products extends Component {
+//     state = {
+//         products: [
+//             { id: 1, name: 'React', price: '99 $' },
+//             { id: 2, name: 'Vue', price: '99 $' },
+//             { id: 3, name: 'JavaScript', price: '90 $' }
+//         ]
+//     }
+
+//     changePriceHandler = () => {
+//         this.setState({
+//             products: [
+//                 { id: 1, name: 'React', price: '120 $' },
+//                 { id: 2, name: 'Vue', price: '120 $' },
+//                 { id: 3, name: 'JavaScript', price: '100 $' }
+//             ]
+//         })
+//     }
+
+//     render() {
+//         return (
+//             <div>
+//                 <h1>Products</h1>
+//                 {this.state.products.map(product => <Product name={product.name} price={product.price} key={product.id} />)}
+//                 <button onClick={this.changePriceHandler}>Change price</button>
+//             </div>
+//         );
+//     }
+// }
+
+// Function based component
+// Version 16.8 >
+// 2018 >
+// Use lifecycle and state
 const Products = () => {
-    const products = [
-        { id: 1, name: 'React', price: '99 $' },
+    const [products, setProducts] = useState([
+        { id: 1, name: 'React', price: '100 $' },
         { id: 2, name: 'Vue', price: '99 $' },
         { id: 3, name: 'JavaScript', price: '90 $' }
-    ]
+    ]);
+
+    const changePriceHandler = () => {
+        setProducts([
+            { id: 1, name: 'React', price: '150 $' },
+            { id: 2, name: 'Vue', price: '160 $' },
+            { id: 3, name: 'JavaScript', price: '120 $' }
+        ])
+    }
+
     return (
         <div>
             <h1>Products</h1>
             {products.map(product => <Product name={product.name} price={product.price} key={product.id} />)}
+            <button onClick={changePriceHandler}>Change price</button>
         </div>
     );
 }
